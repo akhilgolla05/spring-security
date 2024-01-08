3fd2b5f698590881678ab4563007815fac1e902d
akhil <gollaakhil1115@gmail.com>
2024-01-08 17:33:57 -0600

    implemented UserDetails

--- src/main/java/com/projects/springsecuritydemo/entities/User.java
+++ src/main/java/com/projects/springsecuritydemo/entities/User.java
@@ -2,11 +2,18 @@ package com.projects.springsecuritydemo.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.SimpleTimeZone;
 
 @Data
 @Entity
 @Table(name="user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +30,36 @@ public class User {
     private Role role;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    //we dont have getPassword() method, as we are implemnting this class with lambok.
+    //As we have Password Filed, so Lambok already created getters and setters for that.
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
