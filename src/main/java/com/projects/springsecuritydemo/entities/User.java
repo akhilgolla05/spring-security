e615fbe37ae443d2a73a6c9f00bceacdb2a052fb
akhil <gollaakhil1115@gmail.com>
2024-01-08 16:54:03 -0600

    added project structure

---
+++ src/main/java/com/projects/springsecuritydemo/entities/User.java
@@ -0,0 +1,4 @@
+package com.projects.springsecuritydemo.entities;
+
+public class User {
+}
---
+++ src/test/java/com/projects/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
